be9c90c8165c8d5b3aaecc656d8a8a99b97222a1
danielchuperca <ciuperciucd51@gmail.com>
2024-11-06 11:53:27 +0100

    inicio

---
+++ Ej27/src/ej27/Ej27.java
@@ -0,0 +1,10 @@
+package ej27;
+
+public class Ej27 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Ej27/src/ej27/Funciones.java
@@ -0,0 +1,69 @@
+package ej27;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Funciones {
+	public static boolean esInt(String num1) {
+		try {
+		int num=Integer.parseInt(num1);
+		return true;
+		}catch (Exception ex) {
+			return false;
+		}
+	}
+	public static boolean esDouble(String num1) {
+		try {
+		double num=Double.parseDouble(num1);
+		return true;
+		}catch (Exception ex) {
+			return false;
+		}
+	}
+	public static int dimeEntero(String texto, Scanner sc) {
+		do {
+		try {
+		
+		System.out.println(texto);
+		String s1=sc.nextLine();
+		int num=Integer.parseInt(s1);
+		return num;
+		}catch (Exception ex) {
+			System.out.println("formato incorrecto");
+		}
+		
+		}while(true);
+		}
+	public static double dimeDouble(String texto, Scanner sc) {
+		do {
+		try {
+		System.out.println(texto);
+		String s1=sc.nextLine();
+		double num=Double.parseDouble(s1);
+		return num;
+		}catch (Exception ex) {
+			System.out.println("Formato incorrecto");
+		}
+	}while(true);
+		
+
+}
+	public static LocalDate dimeFecha(String texto, Scanner sc) {
+		do {
+			try {
+		System.out.println(texto);
+		String s=sc.nextLine();
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate fecha=LocalDate.parse(s, formato);
+
+		return fecha;
+		
+		}catch(Exception ex) {
+			System.out.println("introduce bien la fecha");
+			
+		}
+		}while(true);
+
+	}
+}
